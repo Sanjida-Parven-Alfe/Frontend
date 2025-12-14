f1ed1aaec26015cc8fb0081ea8e87c987cdb670e
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-15 02:37:32 +0600

    Footer commit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Shared/Navbar'
+import Footer from '../components/Shared/Footer'
 
 
 const MainLayout = () => {
@@ -10,7 +11,7 @@ const MainLayout = () => {
       <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div>
-
+      <Footer />
     </div>
   )
 }
