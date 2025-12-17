dbdb6b47839de73952fb883f4ca6e4cd138b9b9e
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-17 23:37:28 +0600

    auth add'

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/Routes' 
+import { router } from './routes/Routes'
+import AuthProvider from './providers/AuthProvider'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 )
